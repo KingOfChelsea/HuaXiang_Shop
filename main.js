2a7e7b76160e02c4b44b30226405035a30303fbb
KingOfChelsea <xuzhenyu3333@gmail.com>
2024-07-11 14:20:46 +0800

    完成home首页开发

--- main.js
+++ main.js
@@ -3,11 +3,39 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
+// 生产提示
 Vue.config.productionTip = false
+
+//导入网络请求的包
+import {$http} from '@escook/request-miniprogram'
+uni.$http = $http
+//请求根路径
+$http.baseUrl = ' https://api-hmugo-web.itheima.net'
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title:"数据加载中..."
+  })
+}
+//响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500){
+  uni.showToast({
+      title,
+      duration,
+      icon: 'none',
+    })
+}
+
+
 App.mpType = 'app'
 const app = new Vue({
   ...App
 })
+
 app.$mount()
 // #endif
 
---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    11111
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
